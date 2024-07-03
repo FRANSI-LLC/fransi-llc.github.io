875a653aff3a4ac4e3cd6245d19fa73b6fe79d38
Nataniel Jayaseelan <63678990+Ironspidr@users.noreply.github.com>
2024-07-02 22:17:00 -0700

    path fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import Home from './pages/Home';
 import Careers from './pages/careers';
 import {
-    BrowserRouter as Router,
+    BrowserRouter,
     Routes,
     Route,
 } from "react-router-dom";
 import FAQs from './pages/faqs';
 import ContactPage from './pages/contact';
 
+
 export default function App() {
   return (
-    <Router>
+    <div>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/careers" element={<Careers />} />
           <Route path="/faqs" element={<FAQs />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
-    </Router>
+      </BrowserRouter>
+    </div>
+    
   );
 }
