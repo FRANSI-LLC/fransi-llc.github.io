57b3d5f55a42b173850dbab320fb7122424ebf6e
Nataniel Jayaseelan <63678990+Ironspidr@users.noreply.github.com>
2024-07-02 17:52:31 -0700

    path fix #4

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </React.StrictMode>
 );
